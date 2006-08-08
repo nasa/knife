76eae2909705009010534829b8fc051303f82c42
Mike Park <Mike.Park@nasa.gov>
2006-08-08 12:09:29 +0000

    keep it small

--- ruby/triangle.rb
+++ ruby/triangle.rb
@@ -1,10 +1,15 @@
 
 class Triangle
 
- attr_reader :nodes
+ attr_reader :nodes, :children
 
  def initialize(n0,n1,n2)
   @nodes = [n0, n1, n2]
+  @children = [[0,1,2]]
+ end
+
+ def cut_with(n0,n1,n2)
+  false
  end
 
 end
