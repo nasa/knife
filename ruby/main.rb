4d568ef1f546830c79b326b241119791a70afc4d
Mike Park <Mike.Park@nasa.gov>
2006-08-08 13:12:18 +0000

    driver

---
+++ ruby/main.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+
+refine_path = File.expand_path("~/GRIDEX/refine/src")
+
+$:.push refine_path
+
+require 'triangle'
+
+require 'Adj/Adj'
+require 'Line/Line'
+require 'Sort/Sort'
+require 'Grid/Grid'
+require 'GridMath/GridMath'
+require 'Near/Near'
+
+surface_grid = Grid.from_FAST File.expand_path("~/GRIDEX/refine/test/om6_inv08.fgrid")
+
+cut_surface = Array.new
+surface_grid.nface.times do |face_index|
+ face = surface_grid.face(face_index)
+ if ( 1 == face[3] || 2 == face[3] )
+  cut_surface << Triangle.new(surface_grid.nodeXYZ(face[0]),
+                              surface_grid.nodeXYZ(face[1]),
+                              surface_grid.nodeXYZ(face[2]) )
+ end
+end
+
+puts "cut surface has #{cut_surface.size} triangles"
+
+start_time = Time.now
+near_list = Array.new(cut_surface.size)
+cut_surface.each_with_index do |triangle, index|
+ center = triangle.center
+ diameter = triangle.diameter
+ near_list[index] = Near.new(index,center[0],center[1],center[2],diameter)
+end
+cut_tree = near_list.first
+near_list.each_index do |index|
+ cut_tree.insert(near_list[index]) if index > 0
+end
+puts "the tree built in #{Time.now-start_time} sec"
+
+volume_grid = Grid.from_FAST File.expand_path("~/GRIDEX/refine/test/om6box.fgrid")
+
+volume = Array.new
+volume_grid.ncell.times do |cell_index|
+ cell = volume_grid.cell(cell_index)
+ volume << Polyhedron.new(volume_grid.nodeXYZ(cell[0]),
+                          volume_grid.nodeXYZ(cell[1]),
+                          volume_grid.nodeXYZ(cell[2]),
+                          volume_grid.nodeXYZ(cell[3]) )
+end
+puts "volume has #{volume.size} tetes"
+
+start_time = Time.now
+count = 0
+volume.each do |polyhedron|
+ count += 1 ; puts "#{count} of #{volume.size}" if count.divmod(100)[1]==0
+ center = polyhedron.center
+ diameter = polyhedron.diameter
+ probe = Near.new(-1,center[0],center[1],center[2],diameter)
+ cut_tree.touched(probe).each do |index|
+  tool = cut_surface[index]
+  begin
+  polyhedron.cut_with(tool)
+  rescue RuntimeError
+   puts "#$! raised at "+polyhedron.center.join(',')
+  end
+ end
+end
+puts "the cuts required #{Time.now-start_time} sec"
+
+cuts = 0
+File.open('cut_om6.t','w') do |f|
+ f.print cut_surface.first.tecplot_header
+ volume.each do |polyhedron| 
+  if polyhedron.has_been_cut?
+   polyhedron.finalize_cuts
+   cuts += 1
+   polyhedron.exterior.each { |tri| f.print tri.tecplot_zone('int') }
+   polyhedron.interior.each { |tri| f.print tri.tecplot_zone('ext') }
+  end
+ end
+end
+puts "#{cuts} tets cut of #{volume.size}"
+
---
+++ ruby/polyhedron.rb
@@ -0,0 +1,42 @@
+
+require 'triangle'
+
+class Polyhedron
+
+ attr_reader :exterior, :interior
+ attr_reader :center, :diameter
+
+ def initialize(n0,n1,n2,n3)
+  @exterior = [ Triangle.new(n1,n3,n2),
+                Triangle.new(n0,n2,n3),
+                Triangle.new(n0,n3,n1),
+                Triangle.new(n0,n1,n2)]
+  @interior = Array.new
+  @center = [ (n0[0]+n1[0]+n2[0]+n3[0])*0.25,
+              (n0[1]+n1[1]+n2[1]+n3[1])*0.25,
+              (n0[2]+n1[2]+n2[2]+n3[2])*0.25 ]
+  l2 = (n0[0]-@center[0])**2 + 
+       (n0[1]-@center[1])**2 + 
+       (n0[2]-@center[2])**2 
+  d2 = (n1[0]-@center[0])**2 + 
+       (n1[1]-@center[1])**2 + 
+       (n1[2]-@center[2])**2
+  l2 = d2 if d2 > l2
+  d2 = (n2[0]-@center[0])**2 + 
+       (n2[1]-@center[1])**2 + 
+       (n2[2]-@center[2])**2
+  l2 = d2 if d2 > l2
+  d2 = (n3[0]-@center[0])**2 + 
+       (n3[1]-@center[1])**2 + 
+       (n3[2]-@center[2])**2
+  l2 = d2 if d2 > l2
+  @diameter = 1.0001*Math::sqrt(l2)
+ end
+
+ def cut_with(triangle)
+  @exterior.each do |face|
+   face.cut_with(triangle)
+  end
+ end
+
+end
