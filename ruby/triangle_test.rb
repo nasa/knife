87c3dccbb8584a8ba47d19401a6d6472dfc2ed33
Mike Park <Mike.Park@nasa.gov>
2006-08-08 12:50:27 +0000

    test cutter

--- ruby/triangle_test.rb
+++ ruby/triangle_test.rb
@@ -5,6 +5,12 @@ require 'triangle'
 
 class TestTriangle < Test::Unit::TestCase
 
+ def array_in_delta(a,b,tol=1.0e-14)
+  a.size.times do |i|
+   assert_in_delta a[i], b[i], tol, "element #{i}"
+  end
+ end
+
  def set_up
   @n0 = [0.0, 0.0, 0.0]
   @n1 = [1.0, 0.0, 0.0]
@@ -13,50 +19,11 @@ class TestTriangle < Test::Unit::TestCase
  end
  def setup; set_up; end
 
- def test_initial_nodes_and_children
+ def test_initial_nodes
   assert_equal 3, @triangle.nodes.size
   assert_equal @n0, @triangle.nodes[0]
   assert_equal @n1, @triangle.nodes[1]
   assert_equal @n2, @triangle.nodes[2]
-  assert_equal 1, @triangle.children.size
-  assert_equal [0,1,2], @triangle.children[0]
- end
-
- def test_add_node
-  
- end
-
- def test_cut_with_no_change
-  n0 = [2.0, -1.0, -1.0]
-  n1 = [2.0,  1.5, -1.0]
-  n2 = [2.0,  1.5,  1.0]
-  assert_equal false, @triangle.cut_with(n0,n1,n2)
-  assert_equal 3, @triangle.nodes.size
-  assert_equal 1, @triangle.children.size
- end
-
- def test_cut_with_into_two_pieces
-  # 2
-  # | \  
-  # |   +
-  # |   | \
-  # 0 - + - 1
-  n0 = [0.8, -1.0,  1.0]
-  n1 = [0.8, -1.0, -2.0]
-  n2 = [0.8,  2.0,  1.0]
-  assert_equal true, @triangle.cut_with(n0,n1,n2)
-  assert_equal [0.8, 0.0, 0.0], @triangle.nodes[3]
-  assert_equal [0.8, 0.2, 0.0], @triangle.nodes[4]
- end
-
- def test_one_norm
-  n0 = [0.3, 0.3, 0.0]
-  n1 = [0.5, 0.5, 0.2]
-  tol = 1.0e-14
-  assert_in_delta 0.0, @triangle.one_norm(n0,n0), tol
-  assert_in_delta 0.0, @triangle.one_norm(n1,n1), tol
-  assert_in_delta 0.6, @triangle.one_norm(n0,n1), tol
-  assert_in_delta 0.6, @triangle.one_norm(n0,n1), tol
  end
 
  def test_add_unique_node
@@ -69,5 +36,117 @@ class TestTriangle < Test::Unit::TestCase
   assert_equal 4, @triangle.nodes.size
  end
 
+ def test_add_node
+  
+ end
+
+
+ def test_splits_nil_when_misses_top
+  n0 = [0.3,  0.3, -1.0]
+  n1 = [0.3,  0.3, -2.0]
+  assert_nil @triangle.splits(n0,n1)
+  assert_nil @triangle.splits(n1,n0)
+ end
+
+ def test_splits_nil_when_misses_bottom
+  n0 = [0.3,  0.3, -1.0]
+  n1 = [0.3,  0.3, -2.0]
+  assert_nil @triangle.splits(n0,n1)
+  assert_nil @triangle.splits(n1,n0)
+ end
+
+ def test_splits_nil_when_misses_side
+  n0 = [1.0,  1.0,  1.0]
+  n1 = [1.0,  1.0, -1.0]
+  assert_nil @triangle.splits(n0,n1)
+  assert_nil @triangle.splits(n1,n0)
+ end
+
+ def test_splits_returns_a_node_at_center
+  n0 = [0.3,  0.3,  1.0]
+  n1 = [0.3,  0.3, -1.0]
+  target = [0.3,  0.3, 0.0]
+  tol = 1.0e-14
+  ans = @triangle.splits(n0,n1)
+  assert_in_delta target[0], ans[0], tol
+  assert_in_delta target[1], ans[1], tol
+  assert_in_delta target[2], ans[2], tol
+  ans = @triangle.splits(n1,n0)
+  assert_in_delta target[0], ans[0], tol
+  assert_in_delta target[1], ans[1], tol
+  assert_in_delta target[2], ans[2], tol
+ end
+
+ def test_splits_returns_a_node_at_offset
+  n0 = [0.3,  0.3,  0.5]
+  n1 = [0.3,  0.3, -1.5]
+  target = [0.3,  0.3, 0.0]
+  tol = 1.0e-14
+  ans = @triangle.splits(n0,n1)
+  assert_in_delta target[0], ans[0], tol
+  assert_in_delta target[1], ans[1], tol
+  assert_in_delta target[2], ans[2], tol
+  ans = @triangle.splits(n1,n0)
+  assert_in_delta target[0], ans[0], tol
+  assert_in_delta target[1], ans[1], tol
+  assert_in_delta target[2], ans[2], tol
+ end
+
+
+ def test_cut_with_no_change
+  n0 = [2.0, -1.0, -1.0]
+  n1 = [2.0,  1.5, -1.0]
+  n2 = [2.0,  1.5,  1.0]
+  assert_equal false, @triangle.cut_with(Triangle.new(n0,n1,n2))
+  assert_equal 3, @triangle.nodes.size
+ end
+
+ def test_cut_with_into_two_pieces
+  # 2
+  # | \  
+  # |   +
+  # |   | \
+  # 0 - + - 1
+  n0 = [0.8, -1.0,  1.0]
+  n1 = [0.8, -1.0, -2.0]
+  n2 = [0.8,  2.0,  1.0]
+  assert_equal true, @triangle.cut_with(Triangle.new(n0,n1,n2))
+  array_in_delta [0.8, 0.0, 0.0], @triangle.nodes[3]
+  array_in_delta [0.8, 0.2, 0.0], @triangle.nodes[4]
+ end
+
+ def test_one_norm
+  n0 = [0.3, 0.3, 0.0]
+  n1 = [0.5, 0.5, 0.2]
+  tol = 1.0e-14
+  assert_in_delta 0.0, @triangle.one_norm(n0,n0), tol
+  assert_in_delta 0.0, @triangle.one_norm(n1,n1), tol
+  assert_in_delta 0.6, @triangle.one_norm(n0,n1), tol
+  assert_in_delta 0.6, @triangle.one_norm(n0,n1), tol
+ end
+
+ def test_volume6
+  tol = 1.0e-16
+  assert_in_delta( 1.0, @triangle.volume6([0.0,0.0,0.0],
+                                          [1.0,0.0,0.0],
+                                          [0.0,1.0,0.0],
+                                          [0.0,0.0,1.0]), tol )
+  assert_in_delta(-1.0, @triangle.volume6([0.0,0.0,0.0],
+                                          [1.0,0.0,0.0],
+                                          [0.0,1.0,0.0],
+                                          [0.0,0.0,-1.0]), tol )
+  assert_in_delta(-1.0, @triangle.volume6([0.0,0.0,0.0],
+                                          [0.0,1.0,0.0],
+                                          [1.0,0.0,0.0],
+                                          [0.0,0.0,1.0]), tol )
+  assert_in_delta(-1.0, @triangle.volume6([1.0,0.0,0.0],
+                                          [0.0,0.0,0.0],
+                                          [0.0,1.0,0.0],
+                                          [0.0,0.0,1.0]), tol )
+  assert_in_delta(-1.0, @triangle.volume6([0.0,0.0,0.0],
+                                          [1.0,0.0,0.0],
+                                          [0.0,0.0,1.0],
+                                          [0.0,1.0,0.0]), tol )
+ end
 
 end
