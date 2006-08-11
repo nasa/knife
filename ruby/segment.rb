4e323d5b4d9251a58b874d7db86d6fb57520d85e
Mike Park <Mike.Park@nasa.gov>
2006-08-11 14:43:21 +0000

    start a segment

---
+++ ruby/segment.rb
@@ -0,0 +1,10 @@
+# defined between two nodes
+
+class Segment < Array
+
+ def initialize(node0, node1)
+  self << node0
+  self << node1
+ end
+
+end
---
+++ ruby/segment_test.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# unit test for segment
+
+require 'test/unit'
+require 'node'
+require 'segment'
+
+class TestSegment < Test::Unit::TestCase
+ 
+ TOL = 1.0e-14
+
+ def test_initialize
+  node0 = Node.new(0.0, 0.1, 0.2)
+  node1 = Node.new(1.0, 1.1, 1.2)
+
+  segment = Segment.new(node0,node1)
+
+  assert segment.is_a?(Segment), "not a segment"
+
+  assert_equal node0, segment[0]
+  assert_equal node1, segment[1]
+ end
+
+end
