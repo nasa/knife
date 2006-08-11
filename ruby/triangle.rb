c3ede7394f283003883b70f3d5fc230ed300e5e2
Mike Park <Mike.Park@nasa.gov>
2006-08-11 16:40:19 +0000

    implementd node

--- ruby/triangle.rb
+++ ruby/triangle.rb
@@ -8,4 +8,17 @@ class Triangle < Array
   self << segment2
  end
 
+ def node(index)
+  case index
+  when 0
+   self[2][0]
+  when 1
+   self[0][0]
+  when 2
+   self[1][0]
+  else
+   nil
+  end
+ end
+
 end
--- ruby/triangle_test.rb
+++ ruby/triangle_test.rb
@@ -6,6 +6,9 @@ require 'node'
 require 'segment'
 require 'triangle'
 
+# todo
+# - test that segments from a continuous loop
+
 class TestTriangle < Test::Unit::TestCase
  
  TOL = 1.0e-14
@@ -28,6 +31,21 @@ class TestTriangle < Test::Unit::TestCase
   assert_equal segment2, triangle[2]
  end
 
- # test that segments from a continuous loop
+ def test_node
+  node0 = Node.new(0.0, 0.1, 0.2)
+  node1 = Node.new(1.0, 1.1, 1.2)
+  node2 = Node.new(2.0, 2.1, 2.2)
+
+  segment0 = Segment.new(node1,node2)
+  segment1 = Segment.new(node2,node0)
+  segment2 = Segment.new(node0,node1)
+
+  triangle = Triangle.new(segment0,segment1,segment2)
+  assert_nil triangle.node(-1)
+  assert_nil triangle.node(3)
+  assert_equal node0, triangle.node(0)
+  assert_equal node1, triangle.node(1)
+  assert_equal node2, triangle.node(2)
+ end
 
 end
