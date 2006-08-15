e4a3985f59d87e9e283615da71b9ce8cd641c5a0
Mike Park <Mike.Park@nasa.gov>
2006-08-15 18:25:30 +0000

    up to tree make

--- ruby/main.rb
+++ ruby/main.rb
@@ -4,9 +4,10 @@ refine_path = File.expand_path("~/GRIDEX/refine/src")
 
 $:.push refine_path
 
-require 'polyhedron'
-require 'triangle'
+require 'node'
+require 'segment'
 
+# for Grid...
 require 'Adj/Adj'
 require 'Line/Line'
 require 'Sort/Sort'
@@ -14,8 +15,11 @@ require 'Grid/Grid'
 require 'GridMath/GridMath'
 require 'Near/Near'
 
+# read in the grid
 surface_grid = Grid.from_FAST File.expand_path("~/GRIDEX/refine/test/om6_inv08.fgrid")
 
+# create a global to local index for surface nodes
+triangles = 0
 surface_g2l = Array.new(surface_grid.nnode)
 surface_grid.nface.times do |face_index|
  face = surface_grid.face(face_index)
@@ -23,8 +27,11 @@ surface_grid.nface.times do |face_index|
   surface_g2l[face[0]] = true
   surface_g2l[face[1]] = true
   surface_g2l[face[2]] = true
+  triangles += 1
  end
 end
+
+puts "cut surface has #{traingles} triangless"
 
 node = 0
 surface_grid.nnode.times do |node_index|
@@ -36,6 +43,18 @@ end
 
 puts "cut surface has #{node} nodes"
 
+# create surface node objects
+surface_node = Array.new(node)
+node = 0
+surface_grid.nnode.times do |node_index|
+ if surface_g2l[node_index]
+  xyz = surface_grid.nodeXYZ(node_index)
+  surface_node[node] = Node.new(xyz[0],xyz[1],xyz[2])
+  node += 1
+ end
+end
+
+# count unique segments
 segment = Array.new(node)
 node.times { |i| segment[i] = Array.new(node) }
 
@@ -46,15 +65,59 @@ surface_grid.nface.times do |face_index|
   face[0] = surface_g2l[face[0]]
   face[1] = surface_g2l[face[1]]
   face[2] = surface_g2l[face[2]]
+
   node0 = [face[0],face[1]].min
   node1 = [face[0],face[1]].max
   unless segment[node0][node1]
-   segment[node0][node1] = segments
+   segment[node0][node1] = Segment.new(surface_node[node0],surface_node[node1])
    segments++
   end
+
+  node0 = [face[1],face[2]].min
+  node1 = [face[1],face[2]].max
+  unless segment[node0][node1]
+   segment[node0][node1] = Segment.new(surface_node[node0],surface_node[node1])
+   segments++
+  end
+
+  node0 = [face[2],face[0]].min
+  node1 = [face[2],face[0]].max
+  unless segment[node0][node1]
+   segment[node0][node1] = Segment.new(surface_node[node0],surface_node[node1])
+   segments++
+  end
+
  end
 end
 
+puts "cut surface has #{segments} segments"
+
+cut_surface = Array.new(triangles)
+
+triangles = 0
+surface_grid.nface.times do |face_index|
+ face = surface_grid.face(face_index)
+ if ( 1 == face[3] || 2 == face[3] )
+  face[0] = surface_g2l[face[0]]
+  face[1] = surface_g2l[face[1]]
+  face[2] = surface_g2l[face[2]]
+
+  node0 = [face[0],face[1]].min
+  node1 = [face[0],face[1]].max
+  segment2 = segment[node0][node1]
+
+  node0 = [face[1],face[2]].min
+  node1 = [face[1],face[2]].max
+  segment0 = segment[node0][node1]
+
+  node0 = [face[2],face[0]].min
+  node1 = [face[2],face[0]].max
+  segment1 = segment[node0][node1]
+
+  cut_surface[triangles] = Triangle.new(segment0,segment1,segment2)
+  triangles += 1
+ end
+end
 
 start_time = Time.now
 near_list = Array.new(cut_surface.size)
