9cabd4c8310b769d1c87d3ff1d3f2f52619041a0
Mike Park <Mike.Park@nasa.gov>
2006-08-15 14:15:32 +0000

    to add bary

--- ruby/subtri.rb
+++ ruby/subtri.rb
@@ -10,4 +10,26 @@ class Subtri
   @n2 = n2
  end
 
+ def barycentric(node)
+  u0 = n1.v-n0.v
+  u1 = n1.w-n0.w
+  v0 = n2.v-n0.v
+  v1 = n2.w-n0.w
+  w0 = node.v-n0.v
+  w1 = node.w-n0.w
+
+  uv = u0*v0+u1*v1
+  wv = w0*v0+w1*v1
+  wu = w0*u0+w1*u1
+
+  uu = u0*u0+u1*u1
+  vv = v0*v0+v1*v1
+
+  denom = uv*uv - uu*vv
+  s = (uv*wv-vv*wu)/denom;
+  t = (uv*wu-uu*wv)/denom;
+
+  bary = [1.0-s-t,s,t]
+ end
+
 end
--- ruby/subtri_test.rb
+++ ruby/subtri_test.rb
@@ -3,6 +3,7 @@
 
 require 'test/unit'
 require 'subtri'
+require 'subnode'
 
 class TestSubtri < Test::Unit::TestCase
  
@@ -14,6 +15,27 @@ class TestSubtri < Test::Unit::TestCase
   assert_equal "n0", subtri.n0
   assert_equal "n1", subtri.n1
   assert_equal "n2", subtri.n2
+ end
+
+ def test_barycentric
+  subtri = Subtri.new(Subnode.new(1.0,0.0,0.0),
+                      Subnode.new(0.0,1.0,0.0),
+                      Subnode.new(0.0,0.0,1.0))
+
+  tol = 1.0e-14
+
+  u=0.2;v=0.3;w=0.5
+  subnode = Subnode.new(u,v,w)
+  bary = subtri.barycentric(subnode)
+  assert_in_delta u, bary[0], tol
+  assert_in_delta v, bary[1], tol
+  assert_in_delta w, bary[2], tol
+  u=0.5;v=0.1;w=0.4
+  subnode = Subnode.new(u,v,w)
+  bary = subtri.barycentric(subnode)
+  assert_in_delta u, bary[0], tol
+  assert_in_delta v, bary[1], tol
+  assert_in_delta w, bary[2], tol
  end
 
 end
