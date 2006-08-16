387388b2620c58b43e39576a7ed64d43060399ec
Mike Park <Mike.Park@nasa.gov>
2006-08-16 18:09:59 +0000

    added a common test

--- ruby/segment.rb
+++ ruby/segment.rb
@@ -13,4 +13,12 @@ class Segment
   @nodes[index]
  end
 
+ def common_node(segment)
+  return node(0) if node(0)==segment.node(0)
+  return node(0) if node(0)==segment.node(1)
+  return node(1) if node(1)==segment.node(0)
+  return node(1) if node(1)==segment.node(1)
+  nil
+ end
+
 end
--- ruby/segment_test.rb
+++ ruby/segment_test.rb
@@ -23,4 +23,23 @@ class TestSegment < Test::Unit::TestCase
   assert_equal [], segment.intersections
  end
 
+ def test_common_node
+  node0 = Node.new(0.0, 0.1, 0.2)
+  node1 = Node.new(1.0, 1.1, 1.2)
+  node2 = Node.new(2.0, 2.1, 2.2)
+  node3 = Node.new(3.0, 3.1, 3.2)
+  segment01 = Segment.new(node0,node1)
+  segment10 = Segment.new(node1,node0)
+  segment12 = Segment.new(node1,node2)
+  segment21 = Segment.new(node2,node1)
+  segment23 = Segment.new(node2,node3)
+
+  assert_nil segment01.common_node(segment23)
+
+  assert_equal node1, segment01.common_node(segment12)
+  assert_equal node1, segment10.common_node(segment12)
+  assert_equal node1, segment01.common_node(segment21)
+  assert_equal node1, segment10.common_node(segment12)
+ end
+
 end
