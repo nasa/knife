cee08375f9394c0c6875e1438a9f44d00a1e8e78
Mike Park <Mike.Park@nasa.gov>
2006-08-18 19:21:36 +0000

    to trun of subtris

--- ruby/mask.rb
+++ ruby/mask.rb
@@ -7,10 +7,27 @@ class Mask
  def initialize(triangle,reversed=false)
   @triangle = triangle
   @reversed = reversed
+  @trimmed = nil
  end
 
  def method_missing(method, *arguments)
   @triangle.send(method, *arguments)
+ end
+
+ def deactivate_all_subtri
+  @trimmed = Array.new(triangle.subtris.size,true)
+ end
+
+ def subtris
+  if @trimmed
+   out = Array.new
+   triangle.subtris.size.times do |indx|
+    out << triangle.subtris[indx] unless @trimmed[indx]
+   end
+   out
+  else
+   triangle.subtris
+  end
  end
 
 end
