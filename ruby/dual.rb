c6b1dbda0aeb8272cf4a667d509568fdf43003fc
Mike Park <Mike.Park@nasa.gov>
2006-09-27 14:06:31 +0000

    started tet class

--- ruby/dual.rb
+++ ruby/dual.rb
@@ -5,12 +5,45 @@ require 'triangle'
 require 'polyhedron'
 require 'cut'
 
+class Tet
+
+ attr_reader :nodes
+ attr_redaer :center
+
+ def initialize(grid,nodes)
+  @grid = grid
+  @nodes = nodes
+  create_center
+ end
+
+ def create_center
+  xyz0 = @grid.nodeXYZ(@node[0])
+  xyz1 = @grid.nodeXYZ(@node[1])
+  xyz2 = @grid.nodeXYZ(@node[2])
+  xyz3 = @grid.nodeXYZ(@node[3])
+  @center = Node.new( 0.25*(xyz0[0]+xyz1[0]+xyz2[0]+xyz3[0]),
+                      0.25*(xyz0[1]+xyz1[1]+xyz2[1]+xyz3[1]),
+                      0.25*(xyz0[2]+xyz1[2]+xyz2[2]+xyz3[2]) )
+ end
+
+end
+
 class Dual
 
  attr_reader :grid
 
- def Dual.from_grid(volume_grid)
-  Dual.new(Array.new,volume_grid)
+ def Dual.from_grid(grid)
+  poly = Array.new(grid.nnode)
+  grid.nnode.times do |node|
+   poly[node] = Polyhedron.new
+  end
+
+  tets = Array.new(grid.ncell)
+  grid.ncell.times do |cell|
+   tets[cell] = Tet.new(grid.cell(cell))
+  end
+  
+  Dual.new(poly,grid)
  end
 
  def initialize(poly=Array.new, grid=nil)
