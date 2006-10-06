2b47a6d01c21d098dce862a85ed84bdf9d3b03f4
Mike Park <Mike.Park@nasa.gov>
2006-10-06 21:41:46 +0000

    make poly

---
+++ src/poly.c
@@ -0,0 +1,39 @@
+
+/* volume defined by a watertight collection of triangles */
+
+/* $Id$ */
+
+/* Michael A. Park (Mike Park)
+ * Computational AeroSciences Branch
+ * NASA Langley Research Center
+ * Hampton, VA 23681
+ * Phone:(757) 864-6604
+ * Email: Mike.Park@NASA.Gov
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "poly.h"
+
+Poly poly_create( void )
+{
+  Poly poly;
+  
+  poly = (Poly)malloc( sizeof(PolyStruct) );
+  if (NULL == poly) {
+    printf("%s: %d: malloc failed in poly_create\n",
+	   __FILE__,__LINE__);
+    return NULL; 
+  }
+
+  poly->triangles = array_new(4,40);
+
+  return poly;
+}
+
+void poly_free( Poly poly )
+{
+  if ( NULL == poly ) return;
+  array_free( poly->triangles );
+  free( poly );
+}
---
+++ src/poly.h
@@ -0,0 +1,33 @@
+
+/* volume defined by a watertight collection of triangles */
+
+/* $Id$ */
+
+/* Michael A. Park (Mike Park)
+ * Computational AeroSciences Branch
+ * NASA Langley Research Center
+ * Hampton, VA 23681
+ * Phone:(757) 864-6604
+ * Email: Mike.Park@NASA.Gov
+ */
+
+#ifndef POLY_H
+#define POLY_H
+
+#include "knife_definitions.h"
+#include "triangle.h"
+
+BEGIN_C_DECLORATION
+
+typedef struct PolyStruct PolyStruct;
+struct PolyStruct {
+  Array triangles;
+};
+typedef PolyStruct * Poly;
+
+Poly poly_create( void );
+void poly_free( Poly );
+
+END_C_DECLORATION
+
+#endif /* POLY_H */
