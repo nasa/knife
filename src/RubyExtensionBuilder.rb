943c8e676abd5036a81e6b7d815116266174b2cd
Mike Park <Mike.Park@nasa.gov>
2006-10-06 18:15:32 +0000

    added builder

---
+++ src/RubyExtensionBuilder.rb
@@ -0,0 +1,16 @@
+# Ruby C extension build for knife package
+#
+# $Id$
+
+class RubyExtensionBuilder
+
+ def RubyExtensionBuilder.actual_build_of extension
+  system_command = ['ruby build_ruby_extension.rb',extension].join(' ')
+  exit 1 unless system( system_command )
+ end
+
+ def RubyExtensionBuilder.build extension
+  actual_build_of extension
+ end
+
+end
