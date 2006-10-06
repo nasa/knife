3effeea4230eaadb07e7b31edb9751a551a7ac2a
Mike Park <Mike.Park@nasa.gov>
2006-10-06 18:31:29 +0000

    passed

--- src/adj_test.rb
+++ src/adj_test.rb
@@ -17,138 +17,94 @@ class TestAdj < Test::Unit::TestCase
 
  def set_up
   @adj = Adj.new(4,6,3)
-  @bigNode = 10000
+  @big_node = 10000
  end
  def setup ; set_up ; end
 
- def testCreate
+ def test_create
   assert_equal 4, @adj.nnode
   assert_equal 6, @adj.nadj
-  assert_equal 3, @adj.chunkSize
+  assert_equal 3, @adj.chunk_size
  end
 
- def testCreateZeroSize
-  localAdj = Adj.new(0,0,0)
-  assert_equal 1, localAdj.nnode
-  assert_equal 1, localAdj.nadj
-  assert_equal 1, localAdj.chunkSize
+ def test_create_zero_size
+  local_adj = Adj.new(0,0,0)
+  assert_equal 1, local_adj.nnode
+  assert_equal 1, local_adj.nadj
+  assert_equal 1, local_adj.chunk_size
  end
 
- def testRegister
-  assert_nil @adj.register(-1,1)
-  assert_not_nil @adj.register(0,1)
-  assert_nil @adj.register(4,1)
-  assert_nil @adj.register(@bigNode,1)
+ def test_add
+  assert_nil @adj.add(-1,1)
+  assert_not_nil @adj.add(0,1)
+  assert_nil @adj.add(4,1)
+  assert_nil @adj.add(@big_node,1)
  end
 
- def testRegisterAllocatesNewCunk
-  6.times {|i| assert_not_nil @adj.register(0,i)}
+ def test_add_allocates_new_cunk
+  6.times {|i| assert_not_nil @adj.add(0,i)}
   assert_equal 6, @adj.nadj
-  assert_not_nil  @adj.register(1,0)
+  assert_not_nil  @adj.add(1,0)
   assert_equal 9, @adj.nadj
  end
 
- def testReallocKeepsOldRegisteredElements
+ def test_resize_keeps_old_elements
   degree = 10
-  4.times { |node| degree.times { |i| @adj.register(node,10*i+node) } }
+  4.times { |node| degree.times { |i| @adj.add(node,10*i+node) } }
   assert_equal degree, @adj.degree(0) 
   assert_equal degree, @adj.degree(1) 
   assert_equal degree, @adj.degree(2) 
   assert_equal degree, @adj.degree(3) 
  end
 
- def testReallocateNNodesUp
+ def test_resize_nnodes_increase
   assert_equal 4, @adj.nnode
-  assert_not_nil  @adj.realloc(9)
+  assert_not_nil  @adj.resize(9)
   assert_equal 9, @adj.nnode
-  assert_equal @adj,  @adj.register( 8, 899 )
-  assert_not_nil  @adj.realloc(0)
+  assert_equal @adj,  @adj.add( 8, 899 )
+  assert_not_nil  @adj.resize(0)
   assert_equal 1, @adj.nnode
  end
 
- def testReallocateDownWithoutMemoryLeak
-  assert_equal @adj, @adj.register( 2, 299 )
-  assert_not_nil  @adj.realloc(0)
+ def test_resize_down_without_memory_leak
+  assert_equal @adj, @adj.add( 2, 299 )
+  assert_not_nil  @adj.resize(0)
   assert_equal 1, @adj.nnode
-  6.times {|i| assert_not_nil @adj.register(0,i)}
+  6.times {|i| assert_not_nil @adj.add(0,i)}
   assert_equal  6, @adj.nadj
  end
 
- def testIteratorInitialState
-  assert_equal false, @adj.valid
-  assert_equal false, @adj.more
-  assert_equal( -1, @adj.item)
- end
-  
- def testIteratorForBigNode
-  assert_equal @adj,  @adj.first(@bigNode);
-  assert_equal false, @adj.valid;
-  assert_equal @adj,  @adj.first(-1);
-  assert_equal false, @adj.valid;
- end
-
- def testIterator1Element
-  assert_equal @adj,  @adj.register( 2, 299 )
-  assert_equal @adj,  @adj.first(2)
-  assert_equal 299,   @adj.item
-  assert_equal true,  @adj.valid
-  assert_equal false, @adj.more
-  assert_equal @adj,  @adj.next
-  assert_equal false, @adj.valid
- end
-
- def testIterator2Elements
-  assert_equal @adj,  @adj.register( 3, 398 )
-  assert_equal @adj,  @adj.register( 3, 399 )
-  assert_equal @adj,  @adj.first(3);
-  assert_equal true,  @adj.valid
-  assert_equal 399,   @adj.item
-  assert_equal true,  @adj.more
-  assert_equal @adj,  @adj.next
-  assert_equal true,  @adj.valid
-  assert_equal 398,   @adj.item
-  assert_equal false, @adj.more
- end
- 
- def testAbusiveUseOfNext
-  100.times {@adj.next}
-  assert_equal @adj,  @adj.register( 1, 498 )
-  assert_equal @adj,  @adj.register( 1, 499 )
-  assert_equal @adj,  @adj.first(3)
-  100.times {@adj.next}
- end
-
- def testAddAndRemove
+ def test_add_and_remove
   assert_equal false, @adj.exists(1,0)
   assert_nil          @adj.remove(1,0)
-  assert_equal @adj,  @adj.register(1,0)
+  assert_equal @adj,  @adj.add(1,0)
   assert_equal true,  @adj.exists(1,0)
   assert_equal @adj,  @adj.remove(1,0)
   assert_equal false, @adj.exists(1,0)
   assert_nil          @adj.remove(1,0)
  end
  
- def testMultipleExists
+ def test_multiple_exists
   assert_equal false, @adj.exists(1,198)
-  assert_equal @adj,  @adj.register(1,198)
-  assert_equal @adj,  @adj.register(2,198)
-  assert_equal @adj,  @adj.register(1,199)
+  assert_equal @adj,  @adj.add(1,198)
+  assert_equal @adj,  @adj.add(2,198)
+  assert_equal @adj,  @adj.add(1,199)
   
   assert_equal true,  @adj.exists(1,198)
   assert_equal true,  @adj.exists(1,199)
   @adj.remove(1,198)
   assert_equal false, @adj.exists(1,198)
   assert_equal true,  @adj.exists(1,199)
-  @adj.register(1,198)
+  @adj.add(1,198)
   assert_equal true,  @adj.exists(1,198)
   assert_equal true,  @adj.exists(1,199)
  end
 
- def testDegree
+ def test_degree
   assert_equal    0, @adj.degree(0)
-  assert_equal @adj, @adj.register( 0, 299 )
+  assert_equal @adj, @adj.add( 0, 299 )
   assert_equal    1, @adj.degree(0)
-  assert_equal @adj, @adj.register( 0, 298 )
+  assert_equal @adj, @adj.add( 0, 298 )
   assert_equal    2, @adj.degree(0)
  end
 
