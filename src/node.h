56248c856f3be04d524e2fbccfe5861c3cce4279
Mike Park <Mike.Park@nasa.gov>
2006-10-06 20:22:14 +0000

    added a simple node

---
+++ src/node.h
@@ -0,0 +1,29 @@
+
+/* double x-y-z triplet with int index */
+
+/* $Id$ */
+
+/* Michael A. Park (Mike Park)
+ * Computational AeroSciences Branch
+ * NASA Langley Research Center
+ * Hampton, VA 23681
+ * Phone:(757) 864-6604
+ * Email: Mike.Park@NASA.Gov
+ */
+
+#ifndef NODE_H
+#define NODE_H
+
+#include "knife_definitions.h"
+
+BEGIN_C_DECLORATION
+
+typedef struct Node Node;
+struct Node {
+  double x,y,z;
+  int indx;
+};
+
+END_C_DECLORATION
+
+#endif /* NODE_H */
