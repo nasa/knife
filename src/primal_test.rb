6c7150c8728f46bf895c85353dc7da243c7ada2b
Mike Park <Mike.Park@nasa.gov>
2006-10-06 20:00:01 +0000

    added test

--- src/primal_test.rb
+++ src/primal_test.rb
@@ -18,8 +18,25 @@ class TestPrimal < Test::Unit::TestCase
 
  def set_up
   @primal = Primal.new(4,3,1)
+  File.open('primal_test.fgrid','w') do |f|
+   f.puts "4 4 1"
+   f.puts "0.0\n1.0\n0.0\n0.0"
+   f.puts "0.0\n0.0\n1.0\n0.0"
+   f.puts "0.0\n0.0\n0.0\n1.0"
+   f.puts "2 4 3"
+   f.puts "1 3 4"
+   f.puts "1 4 2"
+   f.puts "1 2 3"
+   f.puts "1\n2\n3\n4"
+   f.puts "1 2 3 4"
+  end
  end
  def setup ; set_up ; end
+
+ def tear_down
+  puts "down"
+ end
+ def teardown ; tear_down ; end
 
  def test_create
   assert_equal 4, @primal.nnode
