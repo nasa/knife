fd4668c2e2f6b19138c47095314456dbbdf54d08
Mike Park <Mike.Park@nasa.gov>
2006-10-06 17:01:35 +0000

    stripped down

---
+++ src/knife_main.c
@@ -0,0 +1,22 @@
+/* main driver program for knife package */
+
+/* $Id$ */
+
+/* Michael A. Park (Mike Park)
+ * Computational AeroSciences Branch
+ * NASA Langley Research Center
+ * Hampton, VA 23681
+ * Phone:(757) 864-6604
+ * Email: Mike.Park@NASA.Gov
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "knife_definitions.h"
+
+int main( int argc, char *argv[] )
+{
+
+  return 0;
+}
+
