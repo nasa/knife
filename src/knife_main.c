49269d3a41eb73bcf3b169a08fe1874d0cc04852
Mike Park <Mike.Park@nasa.gov>
2006-10-07 11:26:04 +0000

    read surface grid

--- src/knife_main.c
+++ src/knife_main.c
@@ -12,12 +12,28 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "knife_definitions.h"
 
 int main( int argc, char *argv[] )
 {
-  int i;
-  for(i=0;i<argc;i++) printf("%s\n",argv[i]);
+  int argument;
+
+  char surface_filename[1025];
+
+  sprintf( surface_filename, "not_set" );
+
+  argument = 1;
+  while ( argument < argc )
+    {
+      if( strcmp(argv[argument],"-s") == 0 ) {
+	argument++; 
+	sprintf( surface_filename, "%s", argv[argument] );
+	printf("-s %s\n", surface_filename);
+      }
+
+      argument++;
+    }
   return 0;
 }
 
