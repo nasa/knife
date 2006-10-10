a092337798c75d9d43a5411c3cc801cbc207a9ac
Mike Park <Mike.Park@nasa.gov>
2006-10-10 14:50:05 +0000

    added intersect getter

--- src/cut.h
+++ src/cut.h
@@ -30,6 +30,9 @@ typedef CutStruct * Cut;
 Cut cut_between( Triangle, Triangle );
 void cut_free( Cut );
 
+#define cut_intersection0(cut) ((cut)->intersection0);
+#define cut_intersection1(cut) ((cut)->intersection1);
+
 END_C_DECLORATION
 
 #endif /* CUT_H */
