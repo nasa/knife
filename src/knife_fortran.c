715bd43b962763f24ffec9ca12ee72206e35da02
Mike Park <Mike.Park@nasa.gov>
2006-10-27 23:38:48 +0000

    a broken placehold to fix remotely

--- src/knife_fortran.c
+++ src/knife_fortran.c
@@ -215,3 +215,52 @@ void knife_dual_centroid_volume_( int *node,
   *knife_status = KNIFE_SUCCESS;
 }
 
+void knife_nunber_of_triangles_between_( int *node1, int *node2,
+					 int *nsubtri,
+					 int *knife_status )
+{
+  int n;
+  int edge;
+  Node node;
+
+  poly = domain_poly(domain,(*node1)-1);
+  NOT_NULL(poly, "node1 poly NULL in knife_nunber_of_triangles_between_");
+
+  TRY( primal_edge_between( primal, (*node1)-1,  (*node2)-1, &edge ), 
+       "no edge found by primal_edge_between"); 
+
+  node = domain_node_at_edge_center(domain,edge);
+  NOT_NULL(poly, "edge rimal node NULL in knife_nunber_of_triangles_between_");
+
+  TRY( poly_nsubtri_around( poly, node, nsubtri ), "poly_nsubtri_around" );
+  
+  *knife_status = KNIFE_SUCCESS;
+}
+
+void knife_triangles_between_( int *node1, int *node2,
+			       int *nsubtri,
+			       double *triangle_node0,
+			       double *triangle_node1,
+			       double *triangle_node2,
+			       int *knife_status )
+{
+  int n;
+  int edge;
+  Node node;
+
+  poly = domain_poly(domain,(*node1)-1);
+  NOT_NULL(poly, "node1 poly NULL in knife_triangles_between_");
+
+  TRY( primal_edge_between( primal, (*node1)-1,  (*node2)-1, &edge ), 
+       "no edge found by primal_edge_between"); 
+
+  node = domain_node_at_edge_center(domain,edge);
+  NOT_NULL(poly, "edge rimal node NULL in knife_riangles_between_");
+
+  TRY( poly_subtri_around( poly, node, 
+			   triangle_node0, triangle_node1, triangle_node2 ), 
+       "poly_subtri_around" );
+  
+  *knife_status = KNIFE_SUCCESS;
+}
+
