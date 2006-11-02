7df4222c18a55db5d468451cb85d2c8948920e17
Mike Park <Mike.Park@nasa.gov>
2006-11-02 18:15:02 +0000

    to set frames

--- src/knife_fortran.c
+++ src/knife_fortran.c
@@ -58,6 +58,9 @@ void knife_volume_( int *part_id,
 
   partition = *part_id;
 
+  triangle_set_tecplot_frame( 100*partition);
+  loop_set_tecplot_frame( 100*partition);
+
   volume_primal = primal_create( *nnode, *nface, *ncell );
   NOT_NULL(volume_primal, "volume_primal NULL");
 
