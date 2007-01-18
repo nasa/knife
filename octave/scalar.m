a11d762f8a240ee8e7afb6388ac4eca68ade7df4
Mike Park <Mike.Park@nasa.gov>
2007-01-18 19:31:57 +0000

    plot gradients

--- octave/scalar.m
+++ octave/scalar.m
@@ -1,5 +1,5 @@
 
-n = 40
+n = 10
 
 xi = linspace(0,1,n+1);
 x = 0.5*(xi(1:n)+xi(2:n+1));
@@ -11,16 +11,19 @@ u = ones(1,n);
 
 exact = 1.0+dh-dh*cos(2*pi*x);
 
-for iter = 1:100
+for iter = 1:10
   for subiter = 1:n
-    #g = scalar_grad(x,u);
-    g = zeros(1,n);
+    g = scalar_grad(x,u);
+    #g = zeros(1,n);
     res = scalar_res(x,xi,h,u,g);
     u = u+res/n;
   end
-end
 
+  [x2, u2] = scalar_draw(x,xi,u,g);
+
+  plot(x2,u2,xi,h);axis([0 1 0 2])
+
+end
 res_l2 = norm(res)/n
 error_l2 = norm(u-exact)
-plot(x,u,xi,h);axis([0 1 0 2])
 
