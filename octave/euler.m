512c9c8e29917787f35640f64aa79aca4cc793be
Mike Park <Mike.Park@nasa.gov>
2007-01-18 21:38:52 +0000

    working

--- octave/euler.m
+++ octave/euler.m
@@ -2,14 +2,14 @@
 gamma = 1.4;
 gm1  = gamma - 1.0;
 
-n = 10
+n = 20
 
 xi = linspace(0,1,n+1)';
 x = 0.5*(xi(1:n)+xi(2:n+1));
 
-dh = 0.05
+dh = 0.25
 h = 1.0-dh+dh*cos(2*pi*xi);
-h = h./h;
+
 
 vol = (xi(2:n+1)-xi(1:n)).*0.5.*(h(2:n+1)+h(1:n));
 
@@ -21,15 +21,16 @@ e0 = p0/gm1 + 0.5*rho0*u0*u0;
 
 cu = [ rho0*ones(n,1) rho0*u0*ones(n,1) e0*ones(n,1) ];
 
-for iter = 1:1
-  for subiter = 1:1
-    res = euler_res(x,xi,h,cu)
-    cu = cu + res;
+for iter = 1:500
+  for subiter = 1:10
+    res = euler_res(x,xi,h,cu);
+    cu = cu + res/n;
+  end
 
-res_l2 = norm(res)/n
+res_l2 = norm(res)/n;
 up = c2p(cu);
 plot(x,up(:,1),"-;rho;",x,up(:,2),"-;u;",x,up(:,3),"-;p;",xi,h,"-;h;");
 axis([0 1 0 2])
 
-  end
+
 end
