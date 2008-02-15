df0746b04f7682a333f19e6c0ad17deebc442321
Mike Park <Mike.Park@nasa.gov>
2008-02-15 18:14:20 +0000

    meshes

---
+++ util/mesh2tri.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+# use admesh to make a off file from an stl file
+# http://www.varlog.com/products/admesh/ 
+
+def vertices(mesh)
+ line_number = mesh.each_with_index do |line, number|
+  break(number) if (line =~ /Vertices/)
+  nil
+ end
+ mesh[line_number+1].to_i
+end
+
+def dump_points(mesh,f)
+ line_number = mesh.each_with_index do |line, number|
+  break(number) if (line =~ /Vertices/)
+  nil
+ end
+ n=mesh[line_number+1].to_i
+ (line_number+2).upto(line_number+1+n) do |number|
+  line = mesh[number]
+  numbers = line.scan(/\S+/)
+  f.puts numbers[0..2].join(' ')
+ end
+end
+
+def dump_triangles(mesh,f)
+ line_number = mesh.each_with_index do |line, number|
+  break(number) if (line =~ /Triangles/)
+  nil
+ end
+ n=mesh[line_number+1].to_i
+ (line_number+2).upto(line_number+1+n) do |number|
+  line = mesh[number]
+  numbers = line.scan(/\S+/)
+  f.puts numbers[0..2].join(' ')
+ end
+end
+
+def dump_faceids(mesh,f)
+ line_number = mesh.each_with_index do |line, number|
+  break(number) if (line =~ /Triangles/)
+  nil
+ end
+ n=mesh[line_number+1].to_i
+ (line_number+2).upto(line_number+1+n) do |number|
+  line = mesh[number]
+  numbers = line.scan(/\S+/)
+  f.puts numbers[3]
+ end
+end
+
+def triangles(mesh)
+ line_number = mesh.each_with_index do |line, number|
+  break(number) if (line =~ /Triangles/)
+  nil
+ end
+ mesh[line_number+1].to_i
+end
+
+mesh = IO.readlines(ARGV[0])
+
+nnode = vertices(mesh)
+ntri = triangles(mesh)
+puts "nodes "+nnode.to_s
+puts "tris  "+ntri.to_s
+
+File.open(ARGV[0]+'.tri','w') do |f|
+ f.puts "#{nnode} #{ntri}"
+ dump_points(mesh,f)
+ dump_triangles(mesh,f)
+ dump_faceids(mesh,f)
+end
