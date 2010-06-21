63d6b48d49ece2b636274a174dfc9b391656aa56
Mike Park <MikePark.rb@GMail.com>
2010-06-21 08:12:20 -0400

    more general for home

--- tri/tri.sh
+++ tri/tri.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-f90=/usr/local/pkgs/g95_0.92_64.S/bin/g95
+f90=missing-g95
+if [ -x /usr/local/bin/g95 ]
+then
+  f90=/usr/local/bin/g95
+fi
+if [ -x /usr/local/pkgs/g95_0.92_64.S/bin/g95 ]
+then
+  f90=/usr/local/pkgs/g95_0.92_64.S/bin/g95
+fi
+
+src=../Linux/src
+
+( cd ${src} && make )
 
 ${f90} -o tri tri.f90 || exit 1
 
@@ -20,7 +32,7 @@ head -1 cylinder-ascii.tri > cylinder-header.tri
 
 od -D -f cylinder-header.tri
 
-~/knife/Linux/bin/knife-convert -i cylinder-unf-big.tri
-~/knife/Linux/bin/knife-convert -i cylinder-unf-little.tri
-~/knife/Linux/bin/knife-convert -i cylinder-header.tri
+${src}/knife-convert -i cylinder-unf-big.tri
+${src}/knife-convert -i cylinder-unf-little.tri
+${src}/knife-convert -i cylinder-header.tri
 
