7579cb6af271f7dda5461abb42fd2468b3d20475
Mike Park <MikePark.rb@GMail.com>
2011-09-02 08:48:31 -0400

    to cleanup unused fun3d build scripts

--- scripts/all_compile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-fun_loc=$HOME/FUN3D/`uname -n`
-
-( cd $fun_loc/LibF90 && make -j 4 ) && \
-( cd $fun_loc/PHYSICS_DEPS/ && make -j 4 ) && \
-( cd $fun_loc/ && make -j 4 )
-
--- scripts/core_compile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-fun_loc=$HOME/FUN3D/`uname -n`
-
-( cd $fun_loc/LibF90 && make -j 4 ) && \
-
-( cd $fun_loc/FUN3D_90/ && /usr/local/pkgs/mpich_intel.8.1/bin/mpif90 -I../LibF90 -I../PHYSICS_DUMMY -I../PHYSICS_DEPS -O2 -ip    -o nodet_mpi  adjust_alpha_module.o agglomerate_module.o agglomerate_tet_surface_module.o check_jacobian_module.o main.o -L../PHYSICS_DEPS -lFUN3DPhysicsDeps -L../PHYSICS_DUMMY -lFUN3DPhysics -L../LibF90 -lFUN3DCore      -Vaxlib -lm )
