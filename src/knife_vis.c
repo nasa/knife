071a3fd338e4e854581b31feea2260183d2afcc0
Mike Park <Mike.Park@NASA.gov>
2017-07-17 11:13:16 -0400

    header and info

--- src/knife_vis.c
+++ src/knife_vis.c
@@ -1,10 +1,19 @@
 
-/* Michael A. Park (Mike Park)
- * Computational AeroSciences Branch
- * NASA Langley Research Center
- * Hampton, VA 23681
- * Phone:(757) 864-6604
- * Email: Mike.Park@NASA.Gov
+/* Copyright 2007 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space
+ * Administration. No copyright is claimed in the United States under
+ * Title 17, U.S. Code.  All Other Rights Reserved.
+ *
+ * The knife platform is licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 #include <stdlib.h>
@@ -23,7 +32,23 @@ int main( int argc, char *argv[] )
   if ( 2 > argc ) 
     {
       printf("usage : %s input.{fgrid|tri}\n", argv[0] );
-      
+      printf("\n");
+      printf("Copyright 2007 United States Government as represented by the\n");
+      printf("Administrator of the National Aeronautics and Space\n");
+      printf("Administration. No copyright is claimed in the United States under\n");
+      printf("Title 17, U.S. Code.  All Other Rights Reserved.\n");
+      printf("\n");
+      printf("The knife platform is licensed under the Apache License, Version\n");
+      printf("2.0 (the \"License\"); you may not use this file except in compliance\n");
+      printf("with the License. You may obtain a copy of the License at\n");
+      printf("http://www.apache.org/licenses/LICENSE-2.0.\n");
+      printf("\n");
+      printf("Unless required by applicable law or agreed to in writing, software\n");
+      printf("distributed under the License is distributed on an \"AS IS\" BASIS,\n");
+      printf("WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or\n");
+      printf("implied. See the License for the specific language governing\n");
+      printf("permissions and limitations under the License.\n");
+
       return 1;
     }
 
